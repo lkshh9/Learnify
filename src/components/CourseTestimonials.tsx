f9bf5130063de13b9ecdf0e2775865625f6a38c0
lokesh <lokeshmarwal007@gmail.com>
2024-07-06 23:27:23 +0530

    name

--- src/components/CourseTestimonials.tsx
+++ src/components/CourseTestimonials.tsx
@@ -5,22 +5,22 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 const courseTestimonials = [
   {
-    quote:'CourseTube has revolutionized the way I learn. Turning YouTube playlists into structured courses without ads is a game-changer. I can focus on my studies without distractions and track my progress effortlessly.',
+    quote:'Learnify has revolutionized the way I learn. Turning YouTube playlists into structured courses without ads is a game-changer. I can focus on my studies without distractions and track my progress effortlessly.',
     name: 'Sarah J',
     title: 'Student'
   },
   {
-    quote:'As an educator, CourseTube has been an invaluable tool. I can easily create and manage courses, and my students love the ad-free YouTube integration. It’s made our online classes much more engaging and organized.',
+    quote:'As an educator, Learnify has been an invaluable tool. I can easily create and manage courses, and my students love the ad-free YouTube integration. It’s made our online classes much more engaging and organized.',
     name: 'Mark T.',
     title: 'Educator'
   },
   {
-    quote:'Balancing work and learning can be tough, but CourseTube makes it so much easier. I can turn my favorite YouTube tutorials into courses that fit my schedule. The progress tracking features help me stay on top of my learning goals.',
+    quote:'Balancing work and learning can be tough, but Learnify makes it so much easier. I can turn my favorite YouTube tutorials into courses that fit my schedule. The progress tracking features help me stay on top of my learning goals.',
     name: 'Emily R.',
     title: 'Professional'
   },
   {
-    quote:'The flexibility and ease of use of CourseTube are unmatched. I can upload custom course content and integrate YouTube playlists seamlessly. It’s perfect for creating a comprehensive learning environment for our users.',
+    quote:'The flexibility and ease of use of Learnify are unmatched. I can upload custom course content and integrate YouTube playlists seamlessly. It’s perfect for creating a comprehensive learning environment for our users.',
     name: 'James L.',
     title: 'Admin'
   },
