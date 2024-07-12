68c01a4ab95400ce64e9eb94d08c87b36fde4af2
lokesh <lokeshmarwal007@gmail.com>
2024-07-12 16:50:36 +0530

    fix issue try2

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions, please reach out to us!</p>
+    </div>
+  )
+}
+
+export default page
